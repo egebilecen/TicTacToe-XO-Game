35de69014e109ad8c953061029029a2520cac1ac
Ege Bilecen <egebilecen@icloud.com>
2017-08-10 11:38:10 +0300

    Added vertical and horizontal control of moves. Fixed some bugs and made some improvements.

--- events.js
+++ events.js
@@ -1,6 +1,9 @@
 document.body.addEventListener("mousedown",(e)=>{
     e.preventDefault();
 });
+document.body.addEventListener("click",(e)=>{
+    e.preventDefault();
+});
 canvas.addEventListener("click",(e)=>{
     let pageMargin = 8;
     let posX = e.pageX - pageMargin;
